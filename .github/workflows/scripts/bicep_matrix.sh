8b024ffd73685081bac3a65ac1151256b910a7ed
mauwii <Mauwii@outlook.de>
2023-08-21 21:35:10 +0200

    update bicep_matrix.sh: - also spot changes to bicepparam files - more comments - print matrix json to stdout as well

--- .github/workflows/scripts/bicep_matrix.sh
+++ .github/workflows/scripts/bicep_matrix.sh
@@ -1,21 +1,35 @@
 #!/usr/bin/env bash
 
-name=()
+# initialize empty arrays
+template_names=()
 template_files=()
 template_paths=()
 template_parameters=()
 
 # loop over changed bicep files but ignore deleted files
 while IFS= read -r bicep; do
+
     # create variables for some checks
-    filename=$(basename "${bicep%.bicep}")
+    filename=$(basename "${bicep%.bicep*}")
+    [[ $filename = *".${GITHUB_BASE_REF-$GITHUB_REF_NAME}"* ]] \
+        && filename="${filename%."${GITHUB_BASE_REF-$GITHUB_REF_NAME}"*}"
     foldername="$(basename "$(dirname "${bicep}")")"
     bicep_path="$(dirname "${bicep}")"
+
     # check if the bicep file is named like the folder
     if [[ "${foldername}" = "${filename}" ]]; then
-        name+=("${filename}")
+
+        # make sure the folder is not already in the array
+        if [[ "${template_paths[*]}" = *" ${bicep_path} "* ]]; then
+            echo "Folder ${bicep_path} already in array"
+            continue
+        fi
+
+        # add current item to the arrays
+        template_names+=("${filename}")
         template_files+=("${filename}.bicep")
-        template_paths+=("$(dirname "${bicep}")")
+        template_paths+=("${bicep_path}")
+
         # check if there is a bicepparam file for the current branch
         if [[ -f "${bicep_path}/${filename}.${GITHUB_REF_NAME}.bicepparam" ]]; then
             template_parameters+=("${filename}.${GITHUB_REF_NAME}.bicepparam")
@@ -24,18 +38,17 @@ while IFS= read -r bicep; do
             template_parameters+=("${filename}.bicepparam}")
         # if no bicepparams where found add null to the array
         else
-            # echo "No parameter file found for ${filename}"
+            echo "No parameter file found for ${filename}"
             template_parameters+=("$null")
         fi
     fi
-done < <(git diff --name-only --diff-filter=d "origin/${GITHUB_BASE_REF:-${GITHUB_REF_NAME}^}" "origin/${GITHUB_HEAD_REF:-${GITHUB_REF_NAME}}" -- '*.bicep' | xargs -0)
+done < <(git diff --name-only --diff-filter=d "origin/${GITHUB_BASE_REF:-${GITHUB_REF_NAME}^}" "origin/${GITHUB_HEAD_REF:-${GITHUB_REF_NAME}}" -- 'azure/*.bicep*' | xargs -0)
 
 # function to create a json from the collected arrays
 createJson() {
     printf "["
-    # for i in "${!template_files[@]}"; do
     for ((i = 0; i < ${#template_files[@]}; i++)); do
-        printf "{\"name\":\"%s\",\"path\":\"%s\",\"bicep\":\"%s\",\"bicepparams\":\"%s\"}" "${name[${i}]}" "${template_paths[${i}]}" "${template_files[${i}]}" "${template_parameters[${i}]}"
+        printf "{\"name\":\"%s\",\"path\":\"%s\",\"bicep\":\"%s\",\"bicepparams\":\"%s\"}" "${template_names[${i}]}" "${template_paths[${i}]}" "${template_files[${i}]}" "${template_parameters[${i}]}"
         # check if we are not at the end of the array
         if [[ $i -lt $((${#template_files[@]} - 1)) ]]; then
             printf ","
@@ -44,7 +57,11 @@ createJson() {
     printf "]"
 }
 
-# matrix=$(createJson | jq -c '{include:[.[]| {bicepdir: .path, template: .bicep, parameter: .bicepparams}]}')
+# create the matrix json
 matrix=$(createJson | jq -c '{include: [.[]| {name: .name, path: .path, template: .bicep, parameter: .bicepparams}]}')
 
+# print the matrix json
+printf "\ncreated matrix:\n%s" "$(echo "${matrix}" | jq)"
+
+# create output variable
 printf "matrix=%s\n" "${matrix}" >>"${GITHUB_OUTPUT}"
